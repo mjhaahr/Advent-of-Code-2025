6e7dda0342cd1002454c38ce905ebb5239a0f7ac
Matthew Haahr <matthew.haahr@gmail.com>
2025-12-04 20:44:27 -0800

    [Day-04] Improved by using a set

--- python/puzzles/day-04/solution.py
+++ python/puzzles/day-04/solution.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import itertools
 
 # Modifying Path to include Repo Directory (for util import)
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
@@ -10,47 +11,59 @@ def puzzle(filename, part2):
     # Zero the Accumulator
     score = 0
     
-    rows = []
+    towels = set()
     
     # Open File
     with open(filename, 'r') as fp:
         # Loop over all lines
+        y = 0
         for line in fp.readlines():
             line = line.strip() 
-            rows.append([c for c in line])
+            for x, c in enumerate(line):
+                if c == '@':
+                    towels.add((x, y))
             
-    grid = utils.DictGrid(rows)
-    
+            y += 1
+            
     lastScore = -1
     
     # loop until the score doesn't change
     while (lastScore != score):
         lastScore = score
         toRemove = []
-        
-        # Loop over the whole grid
-        for cell, val in grid:
-            # If it's a paper towel
-            if (val == '@'):
-                # Check the neighbors
-                count = 0
-                for n in grid.getNeighborsOf8(cell):
-                    count += 1 if n[0] == '@' else 0
+
+        for towel in towels:
+            towelsInNeighbors = 0
+            removable = True
+            # Get all Surrounding Cells
+            for offset in itertools.product([-1, 0, 1], [-1, 0, 1]):
+                # If at the center, skip
+                if offset == (0, 0):
+                    continue
+            
+                newCell = tuple(map(sum, zip(towel, offset)))
+                if newCell in towels:
+                    towelsInNeighbors += 1
                 
-                if count < 4:
-                    score += 1
-                    toRemove.append(cell)
-                    
+                if towelsInNeighbors >= 4:
+                    removable = False
+                    break
+        
+            if removable:
+                score += 1
+                toRemove.append(towel)
+            
         # Remove the ones set aside
         for cell in toRemove:
-            grid.set(cell, '.')
+            towels.remove(cell)
             
         # Run once if part 1
         if not part2:
             break
-                
-    # Return Accumulator    
+            
+    # Return Accumulator  
     print(score)
+    
     
 if __name__ == "__main__":
     # Check number of Arguments, expect 2 (after script itself)
