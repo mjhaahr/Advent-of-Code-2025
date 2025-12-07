e8aac6ba1954dcbff68d9e6518364b1c8be4486c
Matthew Haahr <matthew.haahr@gmail.com>
2025-12-06 22:21:20 -0800

    [Day-07] Solved Part 2 Needed a full multiple refactors, found a simpler solution

--- python/puzzles/day-07/solution.py
+++ python/puzzles/day-07/solution.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath
 # Import Utilities
 import utils
 
+printTree = False
 debug = False
 
 def puzzle(filename, part2):
@@ -14,38 +15,57 @@ def puzzle(filename, part2):
     
     splits = 0
     
-    beams = set()
+    paths = []
+    
+    activeBeams = set()
     
     # Open File
     with open(filename, 'r') as fp:
         # Loop over all lines
+        row = 0
         for line in fp.readlines():
             line = line.strip() 
+            toRemove = set()
             for i, c in enumerate(line):
-                if c == 'S':
-                    beams.add(i)
-                if c == '^' and i in beams:
+                if row == 0:
+                    if c == 'S':
+                        paths.append(1)
+                        activeBeams.add(i)
+                    else:
+                        paths.append(0)
+                
+                if c == '^' and i in activeBeams:
                     splits += 1
-                    beams.remove(i)
                     
                     left = i - 1
                     if left >= 0:
-                        beams.add(left)
+                        activeBeams.add(left)
+                        paths[left] += paths[i]
                 
                     right = i + 1
                     if right < len(line):
-                        beams.add(right)
-            
-            if debug:
-                out = ''.join([c if i not in beams else '|' for i, c in enumerate(line)])
-                print(out, end = "\t  | ")
-                print(beams)
+                        activeBeams.add(right)
+                        paths[right] += paths[i]
+                        
+                    activeBeams.remove(i)
+                    paths[i] = 0
+                                
+            if printTree:
+                out = ''.join(['|' if c == '.' and i in activeBeams else c for i, c in enumerate(line)])
+                if debug:
+                    print(out, end = f"  | {paths}\n")
+                
+            row += 1
     
     if not part2:
         score = splits
+    else:
+        score = sum(paths)
+        
         
     # Return Accumulator    
     print(score)
+        
     
 if __name__ == "__main__":
     # Check number of Arguments, expect 2 (after script itself)
