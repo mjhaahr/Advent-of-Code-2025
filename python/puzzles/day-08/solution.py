2418df05d3cb73469b4015966883999b55c066e3
Matthew Haahr <matthew.haahr@gmail.com>
2025-12-07 22:19:22 -0800

    [Day-08] Solved Part 2

--- python/puzzles/day-08/solution.py
+++ python/puzzles/day-08/solution.py
@@ -30,37 +30,67 @@ def puzzle(filename, part2):
     connections = [(distance(a, b), i, j) for (i, a), (j, b) in itertools.combinations(enumerate(boxes), 2)]
     # print(*connections, sep='\n')
     
-    num = 10 if 'example' in filename else 1000
-    
-    # Get the relevant shortest ones
-    shortest = heapq.nsmallest(num, connections, key=lambda x: x[0])
-    #print(*shortest, sep='\n')
-    
-    # Loop over the shortest to build the circuits
+    # Create the initial list of circuits with just one box
     circuits = [{i} for i in range(len(boxes))]
-    for dist, i, j in shortest:
-        # Where the connections are found
-        iIdx = -1
-        jIdx = -1
     
-        # Loop over circuits to check if there's a circuit containing either key
-        for idx, circuit in enumerate(circuits):
+    if not part2:
+        num = 10 if 'example' in filename else 1000
+    
+        # Get the relevant shortest ones
+        shortest = heapq.nsmallest(num, connections, key=lambda x: x[0])
+        #print(*shortest, sep='\n')
+    
+        # Loop over the shortest to build the circuits
+        for dist, i, j in shortest:
+            # Where the connections are found
+            iIdx = -1
+            jIdx = -1
+    
+            # Loop over circuits to check if there's a circuit containing either key
+            for idx, circuit in enumerate(circuits):
                 if i in circuit:
                     iIdx = idx
                 if j in circuit:
                     jIdx = idx
                     
-        # If the two indices are not the same, add j's circuit to i's circuit and remove j's circuit
-        if iIdx != jIdx:
-            circuits[iIdx].update(circuits[jIdx])
-            circuits.pop(jIdx)
+            # If the two indices are not the same, add j's circuit to i's circuit and remove j's circuit
+            if iIdx != jIdx:
+                circuits[iIdx].update(circuits[jIdx])
+                circuits.pop(jIdx)
         
-    # print(*circuits, sep='\n')
+        # print(*circuits, sep='\n')
+        sizes = sorted([len(c) for c in circuits], reverse=True)
+        score = sizes[0] * sizes[1] * sizes[2]
         
+    else:
+        # Turn the list of connections into a min heap
+        heapq.heapify(connections)
         
-    sizes = sorted([len(c) for c in circuits], reverse=True)
-    score = sizes[0] * sizes[1] * sizes[2]
-        
+        # Loop while there are connections to iterate through (shouldn't be unsolvable, but who knows)
+        while connections:
+            # Pop the next shortest connection
+            dist, i, j = heapq.heappop(connections)
+            
+            iIdx = -1
+            jIdx = -1
+    
+            # Loop over circuits to check if there's a circuit containing either key
+            for idx, circuit in enumerate(circuits):
+                if i in circuit:
+                    iIdx = idx
+                if j in circuit:
+                    jIdx = idx
+                    
+             # If the two indices are not the same, add j's circuit to i's circuit and remove j's circuit
+            if iIdx != jIdx:
+                circuits[iIdx].update(circuits[jIdx])
+                circuits.pop(jIdx)
+                
+                # If circuits is now one long circuit, assign the score and break
+                if len(circuits) == 1:
+                    score = boxes[i][0] * boxes[j][0]
+                    break
+    
     
     # Return Accumulator    
     print(score)
