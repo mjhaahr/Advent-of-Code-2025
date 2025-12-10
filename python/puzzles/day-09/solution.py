97a063a7a59aa3b31eb3b5530511bae13bc59230
Matthew Haahr <matthew.haahr@gmail.com>
2025-12-09 17:31:46 -0700

    [Day-09] Solved Part 2, that was a doozy Thanks u/Boojum for the validity check idea

--- python/puzzles/day-09/solution.py
+++ python/puzzles/day-09/solution.py
@@ -18,16 +18,52 @@ def puzzle(filename, part2):
         # Loop over all lines
         for line in fp.readlines():
             line = line.strip()
-            corners.append([int(i) for i in line.split(',')]) 
-    
+            corners.append([int(i) for i in line.split(',')])
+            
+    areas = [] # Store the areas and what corners make them up, used for part 2
     maxA = 0
     # Loop over all combinations to find the one with the greatest area
     for a, b in itertools.combinations(corners, 2):
         area = calcArea(a, b)
+        areas.append((area, a, b))
         if area > maxA:
             maxA = area
+    
+    # Score for Part 1 is just the max area
+    if not part2:   
+        score = maxA    
+    else:
+        # Sort the areas list, by max val, meaning that the first one seen that's valid will be the largest
+        areas.sort(key=lambda x: x[0], reverse=True)
+        
+        # Loop over all rectangles
+        for (area, (ax, ay), (bx, by)) in areas:
+            # Sort the coordinates so the validity test is always running over the correct points
+            x1, x2 = sorted([ax, bx])
+            y1, y2 = sorted([ay, by])
             
-    score = maxA
+            # Validity is proof by contradiction
+            valid = True
+        
+            # Loop over lines (pairs of corners) (adding the first corner to the end to get a loop)
+            # To check if the rectangle is valid
+            corners.append(corners[0])
+            for (l1x, l1y), (l2x, l2y) in itertools.pairwise(corners):
+                # Sort the lines, so the validity check holds
+                minX, maxX = sorted([l1x, l2x])
+                minY, maxY = sorted([l1y, l2y])
+                
+                # Validity check: if the line is intersected by the boxes bounds or that the box is out of the include area
+                # if invalid, break and try next
+                if not (maxX <= x1 or x2 <= minX or maxY <= y1 or y2 <= minY):
+                    valid = False
+                    break
+            
+            # If the rectangle is valid, assign the score and break
+            if valid:
+                score = area
+                break
+        
     
     # Return Accumulator    
     print(score)
