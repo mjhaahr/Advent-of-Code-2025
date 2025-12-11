347d0dea85912a0c7ba51c74ba5f5dcb1b9ac0e5
Matthew Haahr <matthew.haahr@gmail.com>
2025-12-11 07:36:41 -0700

    [Day-11] Improve Speed by using subpaths for part 2 Relies on the observation that 'fft' and 'dac' appear in that order

--- python/puzzles/day-11/solution.py
+++ python/puzzles/day-11/solution.py
@@ -2,11 +2,14 @@ import sys
 import os
 from collections import defaultdict
 from functools import cache
+import itertools
 
 # Modifying Path to include Repo Directory (for util import)
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 # Import Utilities
 import utils
+
+useSubPaths = True
 
 rack = defaultdict(list)
 def puzzle(filename, part2):
@@ -21,19 +24,32 @@ def puzzle(filename, part2):
             line = line.split(' ')
             # split = ['aaa:', 'bbb', 'bbb']
             rack[line[0][:-1]] = line[1:]
+    
+    
+    if useSubPaths:
+        # Using Sub Paths is slightly faster, but relies on 'fft' occurring before 'dac' (which it does for this case)
+        if not part2:
+            start = 'you' if not part2 else 'svr'
+            score = findAllPaths('you', 'out')
+        else:
+            # Multiply the number of paths between each pair of nodes
+            score = 1
+            for start, end in itertools.pairwise(['svr', 'fft', 'dac', 'out']):
+                score *= findAllPaths(start, end)
+                
+    else:
+        start = 'you' if not part2 else 'svr'
+        score = findAllPaths(start, 'out', part2) 
             
-    # List of paths
-    start = 'you' if not part2 else 'svr'
-    score = findAllPaths(start, part2)
     # Return Accumulator    
     print(score)
 
 # Find the number of paths from a given node to the end
 # Default Part 2 quantities to False so Part 1 can run without interruption
 @cache
-def findAllPaths(current, part2=False, fftSeen=False, dacSeen=False):
+def findAllPaths(current, end, part2=False, fftSeen=False, dacSeen=False):
     # If the current node is the end, exit, return a 1, this is a valid path
-    if current == 'out':
+    if current == end:
         return 1 if not part2 else (fftSeen and dacSeen)
 
     # If the current node is not in the rack, no paths exist, return a 0, not a solution
@@ -45,7 +61,7 @@ def findAllPaths(current, part2=False, fftSeen=False, dacSeen=False):
     for node in rack[current]:
         # Recursively find number of paths from the adjacent node to the end
         if not part2:
-            pathCount += findAllPaths(node)
+            pathCount += findAllPaths(node, end)
         else:
             # If the node is one of the special values, set the flag, 
             if node == 'fft':
@@ -53,7 +69,7 @@ def findAllPaths(current, part2=False, fftSeen=False, dacSeen=False):
             elif node == 'dac':
                 dacSeen = True
                 
-            pathCount += findAllPaths(node, part2, fftSeen, dacSeen)
+            pathCount += findAllPaths(node, end, part2, fftSeen, dacSeen)
     
     return pathCount
     
